8b2fffe52b6f612b0401a018aadb20146c80411d
Oh JiSun <88261608+jisun0415@users.noreply.github.com>
2022-04-03 20:42:57 +0900

    [Bronze IV] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(void)
+{
+	using namespace std;
+
+	double A, B;
+
+	cin >> A;
+	cin >> B;
+	double result = A /B;
+	cout.precision(10);
+	cout << result << endl;
+
+	return 0;
+
+}
