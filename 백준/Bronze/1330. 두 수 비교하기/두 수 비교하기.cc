fbbdec5bcd930527a4e482ccbf0bab6fd9be1832
Oh JiSun <88261608+jisun0415@users.noreply.github.com>
2022-04-05 22:36:07 +0900

    [Bronze IV] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+
+	int A, B;
+
+	cin >> A;
+	cin >> B;
+
+	
+	if (A > B)
+		cout << ">";
+	else if (A < B)
+		cout << "<";
+	else
+	{
+		cout << "==";
+	}
+	return 0;
+
+}
