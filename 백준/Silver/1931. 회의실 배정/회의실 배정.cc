d88c943fa705669465464f59f34cf423e9896718
Oh JiSun <88261608+jisun0415@users.noreply.github.com>
2023-08-04 01:32:40 +0900

    [Silver I] Title: 회의실 배정, Time: 84 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -16,11 +16,12 @@ int main()
     }
     
     sort(A.begin(), A.end());
+    
     int cnt=0;
     int save=0;
     for(int i=0;i<n;i++)
     {
-        if(save<=A[i].second)
+        if(save<=A[i].second)//A[i].second는 새로운 회의 시작 시간이고 save는 이전의 회의끝난 시간. 즉 save가 작거나 같다는 A[i]번째 회의 시작시간이랑 안겹치거나 끝나자마자 시작하는 경우 
         {
             save=A[i].first;
             cnt++;
