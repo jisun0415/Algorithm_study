7b8db597b9563ddc73a7455d4854f0a102d45e91
Oh JiSun <88261608+jisun0415@users.noreply.github.com>
2023-08-11 14:55:21 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,17 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include <queue>
+
 using namespace std;
 
-static int dx[]={0,1,0,-1};
-static int dy[]={1,0,-1,0};
 static int A[101][101];
 static bool visited[101][101]={false};
+static int dx[]={0,1,0,-1};
+static int dy[]={1,0,-1,0};
 static int N,M;
 
-void BFS(int i, int j)
+void bfs(int i, int j)
 {
-    queue<pair<int, int>> myqueue;
-    myqueue.push(make_pair(i,j));
+    queue<pair<int,int>> myqueue;
+    myqueue.push(make_pair(i, j));
     
     while(!myqueue.empty())
     {
@@ -23,42 +26,41 @@ void BFS(int i, int j)
         
         for(int k=0;k<4;k++)
         {
-            int x=now[0] +dx[k];
+            int x=now[0]+dx[k];
             int y=now[1]+dy[k];
             
-            if(x>=0 && y>=0 && x<N && y<M)
+            if(x>=0&&y>=0&&x<N&&y<M)
             {
-                if(A[x][y]!=0&&!visited[x][y]){
+                if(!visited[x][y]&&(A[x][y]!=0))
+                {
                     visited[x][y]=true;
                     A[x][y]=A[now[0]][now[1]]+1;
                     myqueue.push(make_pair(x,y));
                 }
+            
             }
+            
         }
     }
     
 }
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
     cin>>N>>M;
     
     for(int i=0;i<N;i++)
     {
         string s;
         cin>>s;
+        
         for(int j=0;j<M;j++)
         {
             A[i][j]=s[j]-'0';
         }
     }
     
-    BFS(0,0);
-    cout<<A[N-1][M-1]<<"\n";
+    bfs(0,0);
     
+    cout<<A[N-1][M-1]<<"\n";
     
 }
