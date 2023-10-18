7843bc1ac9fab1196fef89cb145dd4ce34b4f63e
Oh JiSun <88261608+jisun0415@users.noreply.github.com>
2023-10-18 14:30:41 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,30 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL); //종종 시간을 초과하는 문제가 위 3개의 구문 추가로 간단히 해결됌.
+    cout.tie(NULL);
     
-    int N,M;
-    int n[100001]={};
-    cin>>N;
-    cin>>M;
+    int a, b;
+    cin >> a >> b;
+    vector<int> S(a,0);
     
-    for(int i=1;i<=N;i++)
+    int N;
+    for(int i=1;i<=a;i++)
     {
-        int tmp;
-        cin>>tmp;
-        n[i]=n[i-1]+tmp;   
-     }
-    
-    for(int s=0;s<M;s++)
-    {
-        int i,j;
-        cin>>i>>j;
-        cout<<n[j]-n[i-1]<<"\n";   
+            cin>>N;
+            S[i]=S[i-1]+N;
+            
     }
     
-    
+    int c,d;
+    for(int i=0;i<b;i++)
+    {
+        cin>>c>>d;
+        cout<<S[d]-S[c-1]<<"\n";
+    }
 }
