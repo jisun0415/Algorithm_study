211606a213ed533f50f55b68630d5c54f0c7194a
Oh JiSun <88261608+jisun0415@users.noreply.github.com>
2023-10-21 12:49:26 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 140 ms, Memory: 6264 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -2,34 +2,33 @@
 #include <vector>
 using namespace std;
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
-    int N,M;
-    cin>>N>>M;
+    int a, b;
+    int n;
+    cin >> a >> b;
+    vector<vector<int>> S(a+1,vector<int>(a+1,0));
     
-    vector<vector<int>> A(N+1,vector<int>(N+1,0));
-    vector<vector<int>> D(N+1,vector<int>(N+1,0));
-    
-    for(int i=1;i<=N;i++)
+    for(int i=1;i<=a;i++)
     {
-        for(int j=1;j<=N;j++)
+        for(int j=1;j<=a;j++)
         {
-            cin>>A[i][j];
-            D[i][j]=D[i][j-1]+D[i-1][j]-D[i-1][j-1]+A[i][j];
+            cin>>n;
+            S[i][j]=S[i][j-1]+S[i-1][j]-S[i-1][j-1]+n;
         }
     }
     
-    for(int i=0;i<M;i++)
+    
+    for(int i=0;i<b;i++)
     {
         int x1,x2,y1,y2;
         cin>>x1>>y1>>x2>>y2;
-        int result=D[x2][y2]-D[x1-1][y2]-D[x2][y1-1]+D[x1-1][y1-1];
-        cout<<result<<"\n";
+        
+        cout<<S[x2][y2]-S[x1-1][y2]-S[x2][y1-1]+S[x1-1][y1-1]<<"\n";
     }
-
-
+    
+    return 0;
 }
